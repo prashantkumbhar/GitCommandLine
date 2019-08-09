ef5cc3369609915bb9dc52cca3bdfe20afc15444
PrashantKumbhar <prashantkumbhar13@gmail.com>
2019-08-09 14:37:47 +0530

    Initial Commit

---
+++ Calc.java
@@ -0,0 +1,17 @@
+
+class Calc{
+	public static void main(String[] args){
+		System.out.println(add(10,5));
+	}	
+	public static int add(int a,int b){
+		return a + b;
+	}
+	public static int subtract(int a,int b){
+		return a - b;
+	}
+	public static int multiply(int a,int b){
+		return a * b;
+	}public static int divide(int a,int b){
+		return a / b;
+	}
+}
